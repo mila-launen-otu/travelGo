598ce3ce21f2798191e75941d7ee69ac1b6b7c5a
cafakleinn <kleinlester.cafa@ontariotechu.net>
2025-03-27 16:16:21 -0400

    Added requires java.desktop (idk what this does but IntelliJ added it

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.example.exp.travelgogui {
     requires javafx.controls;
     requires com.fasterxml.jackson.databind;
     requires com.ctc.wstx;
+    requires java.desktop;
 
     opens com.example.exp.travelgogui to javafx.fxml;
     exports com.example.exp.travelgogui;
